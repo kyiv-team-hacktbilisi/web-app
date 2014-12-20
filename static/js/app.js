aeca3117b9f8dbf916f38b9c8c8a6e11728d3730
Oleksandr <sasha.sochka@gmail.com>
2014-12-20 04:12:16 +0200

    add login page'

--- static/js/app.js
+++ static/js/app.js
@@ -1,1 +1,10 @@
-angular.module('app', [])
+angular.module('app', ['ui.router', 'ngMaterial'])
+    .config(function ($stateProvider, $urlRouterProvider) {
+        $urlRouterProvider.when('', 'login');
+        $stateProvider
+            .state('login', {
+                url: '/login',
+                templateUrl: 'views/login.html'//,
+                //controller: 'PageCtrl'
+            });
+    });
