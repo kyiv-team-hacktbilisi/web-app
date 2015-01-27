728ca409a6534f1d5e398013bc6ca846d3fcd93b
Sviatoslav Sydorenko <wk.cvs.github@sydorenko.org.ua>
2015-01-27 08:50:47 +0200

    Install asyncio if we are dealing w/ Python 3.3

--- setup.py
+++ setup.py
@@ -4,6 +4,9 @@ install_requires = ['redis', 'motor', 'jinja2', 'yuicompressor', 'webassets',
                     'cssmin', 'PyYAML']
 # 'Routes'
 
+if sys.version_info == (3,3):           # Python 3.4 introduced `asyncio` in standard library,
+    install_requires.append('asyncio')  # it was backported for 3.3 as a pypi module
+
 setup(name='scheduler',
       version='0.1',
       author='Sviatoslav Sydorenko',
